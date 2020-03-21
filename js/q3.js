bfd9c4883f518a515f3dfe3d13a13bc281e714d0
Reham-omar <rehamalsobh96@gmail.com>
2020-03-21 22:28:16 +0200

    add

--- js/q3.js
+++ js/q3.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var inputForm = document.getElementById('myForm');
-var content = [];
+var whattodo=[];
+var dateOf=[];
 function formlist(doneToday, date) {
     this.doneToday = doneToday;
     this.date = date;
@@ -14,11 +15,10 @@ inputForm.addEventListener('submit', function (event) {
     var date = event.target.mydate.value;
     // console.log(doneToday);
 
-    var incert = new formlist(doneToday, date);
-    console.log(incert);
-    content.push(incert);
-    // console.log(content);
-    setItem();
+    whattodo.push(doneToday);
+    dateOf.push(date);
+    setItem1();
+    setItem2();
     listofResult();
     inputForm.reset();
 
@@ -27,26 +27,49 @@ inputForm.addEventListener('submit', function (event) {
 });
 
 function  listofResult(){
+    
     var ulEl=document.getElementById('toDo');
-    for (var i=0 ;i<content.length;i++){
+    ulEl.textContent = "";
+    for (var i=0 ;i<whattodo.length;i++){
         var liEl=document.createElement('li');
-        liEl.textContent=`${content[i].doneToday} at ${content[i].date} `;
+        liEl.textContent=`${i+1} - ${whattodo[i]}`;
         ulEl.appendChild(liEl);
+        var pEl=document.createElement('p');
+        pEl.textContent=` ${dateOf[i]}`;
+        ulEl.appendChild( pEl);
+        // var imgEl=document.createElement('img');
+        // img.src =dowenload.png;
+        // liEl.appendChild(imgEl);
     }
 
 }
 
- function setItem(){
-     var datas=JSON.stringify(content);
-     localStorage.setItem('user information' ,datas);
+
+function setItem1(){
+    var whattodotoday=JSON.stringify(whattodo);
+    localStorage.setItem('what to do' , whattodotoday);
+
+}
+function setItem2(){
+    var dateOftoday=JSON.stringify(dateOf);
+    localStorage.setItem('date of' ,dateOftoday);
 
 }
 
-function getItem(){
-    var getresult=localStorage.getItem('result of the form');
-    if (getresult){
-        content=JSON.parse(getresult);
+
+function getItem1(){
+    var getresult1=localStorage.getItem('result 1 of the form');
+    if (getresult1){
+        whattodo=JSON.parse(getresult1);
     }
 
 }
-getItem();
+function getItem2(){
+    var getresult2=localStorage.getItem('result 2 of the form');
+    if (getresult2){
+        dateOf=JSON.parse(getresult2);
+    }
+
+}
+getItem1();
+getItem2();
